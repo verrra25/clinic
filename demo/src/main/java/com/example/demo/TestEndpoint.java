2f98be4d9829a8430373c800e285c63ca29e42c4
Verra <verapost2015@gmail.com>
2026-04-05 17:39:32 +0300

    add test get mapping

---
+++ demo/src/main/java/com/example/demo/TestEndpoint.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestEndpoint {
+    @GetMapping("/hello")
+    public String returnTestString() {
+        return "Hello world";
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("TestEndpoint loaded");
+    }
+}
+
+
+
+
